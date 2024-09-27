db922f7d714de44cf6ae5ae599639270d2fd25a5
Spandan Joshi <spandanj685@gmail.com>
2024-09-27 22:33:42 +0530

    Crew ai

---
+++ 12_crew_ai/crew.py
@@ -0,0 +1,17 @@
+from crewai import Crew, Process
+from agents import blog_researcher, blog_writer
+from tasks import research_task, write_task
+
+crew = Crew(
+  agents=[blog_researcher, blog_writer],
+  tasks=[research_task, write_task],
+  process=Process.sequential,  # Optional: Sequential task execution is default
+  memory=True,
+  cache=True,
+  max_rpm=100,
+  share_crew=True
+)
+
+## start the task execution process with enhanced feedback
+result=crew.kickoff(inputs={'topic':'Returning From The Dead - Hacking Story'})
+print(result)
